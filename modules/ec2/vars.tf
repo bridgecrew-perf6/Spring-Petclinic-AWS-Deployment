c88be4643a1bcf1dcd1dce3feca5435c2c32826a
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2022-01-19 12:54:33 +0000

    removed subnet to enable public IP, added security group for ssh

--- modules/ec2/vars.tf
+++ modules/ec2/vars.tf
@@ -7,5 +7,3 @@ variable "ami_id" {}
 variable "instance_type" {
   default = "t2.micro"
 }
-
-variable "subnet_id" {}
--- prod/main.tf
+++ prod/main.tf
@@ -12,18 +12,16 @@ provider "aws" {
   secret_key = var.secretkey
 }
 
-module "my_vpc" {
-  source      = "../modules/vpc"
-  vpc_cidr    = "192.168.0.0/16"
-  tenancy     = "default"
-  vpc_id      = "${module.my_vpc.vpc_id}"
-  subnet_cidr = "192.168.1.0/24"
-}
-
-module "my_ec2" {
+module "front-end" {
   source        = "../modules/ec2"
   ec2_count     = 1
   ami_id        = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
-  subnet_id     = "${module.my_vpc.subnet_id}"
+}
+
+module "back-end" {
+  source        = "../modules/ec2"
+  ec2_count     = 1
+  ami_id        = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 }
