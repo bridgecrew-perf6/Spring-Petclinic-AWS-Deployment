d77dc295ff0ca73b7898e2e559180ed1f55b3db7
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2022-01-19 12:24:56 +0000

    Terraform creates 1 instance on an IAM User

--- prod/main.tf
+++ prod/main.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.72.0"
+      }
+  }
+}
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-2"
+  access_key = var.accesskey
+  secret_key = var.secretkey
 }
 
 module "my_vpc" {
@@ -13,7 +23,7 @@ module "my_vpc" {
 module "my_ec2" {
   source        = "../modules/ec2"
   ec2_count     = 1
-  ami_id        = "ami-5a8da735"
+  ami_id        = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   subnet_id     = "${module.my_vpc.subnet_id}"
 }
