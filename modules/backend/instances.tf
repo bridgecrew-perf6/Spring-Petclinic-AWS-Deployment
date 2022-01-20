dd2ac7263928fa60a6c29362e6534d2d5c3d3ec3
aidybobs <aidanstark@hotmail.com>
2022-01-20 23:53:08 +0000

    maybe this?

--- modules/backend/instances.tf
+++ modules/backend/instances.tf
@@ -17,13 +17,13 @@ resource "aws_instance" "web" {
   }
 
   provisioner "file" {
-    source      = "../modules/backend/deploy.sh "
+    source      = "~Spring-Petclinic-AWS-Deployment/modules/backend/deploy.sh "
     destination = "/tmp/deploy.sh"
   }  
   provisioner "remote-exec" {
     inline = [
-      "chmod +x ../tmp/deploy.sh",
-      "cd ../tmp/",
+      "chmod +x /tmp/deploy.sh",
+      "cd /tmp/",
       "sh deploy.sh",
     ]
   }
